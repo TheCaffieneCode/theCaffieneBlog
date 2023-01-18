4cbc3a9eaeacc12a0be78471df1a7a6ec3ae951c
the caffeine code <101496484+TheCaffieneCode@users.noreply.github.com>
2023-01-18 12:31:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCaHl4tgtp4A56vaCLY-rq5kP930tw8oVg",
-  authDomain: "myblogsite-6d831.firebaseapp.com",
-  projectId: "myblogsite-6d831",
-  storageBucket: "myblogsite-6d831.appspot.com",
-  messagingSenderId: "137334216142",
-  appId: "1:137334216142:web:2e8be932bad5676216479f",
-  databaseURL: 'https://myblogsite-6d831.firebaseio.com'
+  
 });
 
 const fb = firebase;
